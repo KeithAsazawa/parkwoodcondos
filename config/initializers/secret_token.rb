248f66461ff1fab9ff6da4ed87899ebf37d73644
KeithAsazawa <keith.asazawa@gmail.com>
2013-09-11 13:28:37 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parkwoodcondos::Application.config.secret_token = '7557b33dd381fba93ec6524ba59a21afb407c91d419a4f749f46eb0904e6b71f815fdf497e19288cd6c8b77af0f356e702f996a788c86bf4a844d2f8a59a6978'
